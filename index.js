ac9f84638e61689744303f9e25a559646736782c
Rohan Raj <117286731+HalfBlood-1@users.noreply.github.com>
2024-10-19 14:44:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 for(var i =0 ; i < document.querySelectorAll(".drum").length ; i++){
 document.querySelectorAll("button")[i].addEventListener("click" , function (){
     
-    // var sound = new Audio('sounds/tom-1.mp3');
-    // sound.play();
+    var sound = new Audio('sounds/tom-1.mp3');
+    sound.play();
 
     var dododo = this.innerHTML;
 
